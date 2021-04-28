2a7b31c861ac802241ac905564a7120213007206
EugeneAbongo <abongoeugene@gmail.com>
2021-04-28 21:20:26 +0300

    add everything

--- assets/js/main.js
+++ assets/js/main.js
@@ -39,20 +39,17 @@ sr.reveal('.button',{delay: 200});
 sr.reveal('.home__img',{delay: 400}); 
 sr.reveal('.home__social-icon',{ interval: 200}); 
 
-/*SCROLL ABOUT*/
-sr.reveal('.about__img',{}); 
-sr.reveal('.about__subtitle',{delay: 400}); 
-sr.reveal('.about__text',{delay: 400}); 
+/*SCROLL BUY */
+sr.reveal('.buy__img',{}); 
+sr.reveal('.buy__subtitle',{delay: 400}); 
+sr.reveal('.buy__text',{delay: 400}); 
 
-/*SCROLL SKILLS*/
-sr.reveal('.skills__subtitle',{}); 
-sr.reveal('.skills__text',{}); 
-sr.reveal('.skills__data',{interval: 200}); 
-sr.reveal('.skills__img',{delay: 600});
-
-/*SCROLL WORK*/
-sr.reveal('.work__img',{interval: 200}); 
-
+/*SCROLL RENT*/
+sr.reveal('.rent__subtitle',{}); 
+sr.reveal('.rent__text',{}); 
+sr.reveal('.rent__data',{interval: 200}); 
+sr.reveal('.rent__img',{delay: 600});
+ 
 /*SCROLL CONTACT*/
 sr.reveal('.contact__input',{interval: 200}); 
 
